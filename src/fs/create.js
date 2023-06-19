b070a256634cf43dc540b7a0cd2c523e99d42d0e
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-19 23:25:43 +0300

    feat: implement function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs/promises';
+import url from 'node:url';
+
 const create = async () => {
-    // Write your code here 
+    const path = url.fileURLToPath(new URL('files/fresh.txt', import.meta.url));
+
+    try {
+        await fs.stat(path);
+    } catch(e) {
+        await fs.writeFile(path, 'I am fresh and young');
+        return;
+    }
+
+    throw new Error('FS operation failed');
 };
 
 await create();
