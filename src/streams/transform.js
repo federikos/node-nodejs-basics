a2bb2923bcf30d23bf8dbb2fc57b1b988c7e1246
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-20 23:42:45 +0300

    fix: small nameing fix

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,14 +1,14 @@
 import { Transform, pipeline } from 'node:stream';
 
 const transform = async () => {
-    const transform = new Transform({
+    const transforming = new Transform({
         transform(chunk, _encoding, callback) {
           const chunkStringified = chunk.toString().trim();
           const reversedChunk = chunkStringified.split("").reverse("").join("");
           callback(null, reversedChunk);
         },
       });
-    pipeline(process.stdin, transform, process.stdout, (err) => console.log(err));
+    pipeline(process.stdin, transforming, process.stdout, (err) => console.log(err));
 };
 
 await transform();
