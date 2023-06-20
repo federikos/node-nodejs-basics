98d2d7e28a352f07eba24ab3fef21dc5c00143b7
Fiodar Savutsin <rabinzonim@gmail.com>
2023-06-21 00:10:31 +0300

    feat: implement zlib subtusk

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { promisify } from 'node:util';
+import { pipeline } from 'node:stream';
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import url from 'node:url';
+import path from 'node:path';
+
+const pipe = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+    try {
+        const inputPath = url.fileURLToPath(new URL(
+            path.join('files', 'archive.gz'), 
+            import.meta.url
+        ));
+        const outputPath = url.fileURLToPath(new URL(
+            path.join('files', 'fileToCompress.txt'), 
+            import.meta.url
+        ));
+        const source = createReadStream(inputPath);
+        const destination = createWriteStream(outputPath);
+        await pipe(source, createGunzip(), destination);
+    } catch(err) {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+    }
 };
 
 await decompress();
